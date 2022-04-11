0d712f70bacfe9fd9c4c3e2537e43bb480b71648
ricardo <ricardorbxx1@gmail.com>
2022-04-11 15:42:40 -0300

    very poorly made UI added

--- script.js
+++ script.js
@@ -3,63 +3,43 @@
 let choices = ["rock", "paper", "scissor"];
 let scoreComputer = 0;
 let scorePlayer = 0;
+const resultTag = document.querySelector("#result");
+const playerTag = document.querySelector("#playerChoice");
+const computerTag = document.querySelector("#computerChoice");
+const buttons = document.querySelectorAll("button");
+let choice;
+
+// Function to select the computer move
 
 function computerSelection() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function playerSelection() {
-  let playerChoice = prompt("Choose your move, player!");
-  playerChoice = playerChoice.toLowerCase();
-  while (true) {
-    if (
-      playerChoice != "rock" &&
-      playerChoice != "paper" &&
-      playerChoice != "scissor"
-    ) {
-      playerChoice = prompt("Invalid choice, choose another one!");
-    } else {
-      break;
-    }
-  }
-  return playerChoice;
-}
+buttons.forEach((button) => button.addEventListener("click", theGame));
 
-function game() {
-  let playerChoice = playerSelection();
+function theGame(e) {
   let computerChoice = computerSelection();
+  let playerChoice = e.target.className;
+  playerTag.textContent = `Player: ${playerChoice}`;
+  computerTag.textContent = `Computer: ${computerChoice}`;
   if (
     (computerChoice == "rock" && playerChoice == "scissor") ||
     (computerChoice == "paper" && playerChoice == "rock") ||
     (computerChoice == "scissor" && playerChoice == "paper")
   ) {
     ++scoreComputer;
-    console.log(`
-		  Computer chose: ${computerChoice}  - Score: ${scoreComputer}
-			Player chose: ${playerChoice} - Score: ${scorePlayer}
-			Computer won this round!`);
+    result.textContent = `Computer won this round!`;
   } else if (computerChoice == playerChoice) {
-    console.log(`
-		  Computer chose: ${computerChoice} -  Score: ${scoreComputer}
-			Player chose: ${playerChoice} - Score: ${scorePlayer}
-			It was a draw!`);
+    result.textContent = `It was a draw!`;
   } else {
     ++scorePlayer;
-    console.log(`
-		  Computer chose: ${computerChoice} - Score: ${scoreComputer}
-			Player chose: ${playerChoice} - Score: ${scorePlayer}
-			Player won this round!`);
+    result.textContent = `Player won this round!`;
   }
-}
-
-for (let i = 0; i < 5; i++) {
-  game();
-}
-
-if (scoreComputer > scorePlayer) {
-  alert("Computer won!!! HAHAHAHAHA");
-} else if (scorePlayer > scoreComputer) {
-  alert("Player won!!!");
-} else {
-  alert("It was a draw!!! WTF DUDES");
+	if(scorePlayer === 5){
+		result.textContent = "Player WON!!!"
+		buttons.forEach(button => button.removeEventListener('click', theGame))
+	} else if(scoreComputer === 5){
+		result.textContent = "Computer WON!!!"
+		buttons.forEach(button => button.removeEventListener('click', theGame))
+	}
 }
